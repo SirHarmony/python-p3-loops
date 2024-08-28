122c39a68ee016877d0da63e59247ec5b0825e5d
Harmony <harmonymukolwe@gmail.com>
2024-08-28 14:35:40 +0300

    Adds for loops and while loops

--- lib/looping.py
+++ lib/looping.py
@@ -2,12 +2,26 @@
 
 def happy_new_year():
     # code goes here!
-    pass
+    i=10
+    while i > 0:
+        print(i)
+        i-+1
+    print("Happy New YEar!")
+
 
 def square_integers(int_list):
     # code goes here!
-    pass
+    int_list = [integer * integer for integer in int_list]
 
 def fizzbuzz():
     # code goes here!
-    pass
+    i=1
+    while i <  101:
+        if i % 3 == 0 and i % 5 ==0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
